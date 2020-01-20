3911a994711cb77a972b2ac163fb64ebfa76d874
vincent tran <vincentvtran17@gmail.com>
2020-01-19 22:57:03 -0800

    Node.cs

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Puzzle8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] puzzle =
+            {
+                1, 2, 4,
+                3, 0, 5,
+                7, 6, 8
+            };
+            int[] puzzle1=
+            {
+                        1,2,3,
+                        8,0,4,
+                        7,6,5
+            };
+            int[] puzzle2 =
+            {
+                        2,8,3,
+                        1,6,4,
+                        7,0,5
+            };
+            int[] puzzle3 =
+            {
+                        2,8,3,
+                        1,6,4,
+                        0,7,5
+            };
+            int[] puzzle4 =
+            {
+                        1,2,3,
+                        7,0,4,
+                        6,8,5
+            };
+            int[] puzzle5 =
+            {
+                        2,0,8,
+                        1,6,3,
+                        7,5,4
+            };
+            
+            Node root = new Node(puzzle4);
+            Console.Write("\t Root is: \t");
+            root.PrintPuzzle();
+
+            UniformedSearch ui = new UniformedSearch();
+            List<Node> solutionForBFS = ui.BreathFirstSearch(root);
+            List<Node> solutionForDFS = ui.DepthFirstSearch(root);
+            if (solutionForBFS.Count > 0)
+            {
+                Console.WriteLine("BFS Result");
+                for(int i = solutionForBFS.Count - 1; i > 0; i--)
+                {
+                    solutionForBFS[i].PrintPuzzle();
+                    
+                }
+            }
+            if (solutionForDFS.Count > 0)
+            {
+                Console.WriteLine("DFS Result");
+                for (int i = solutionForDFS.Count - 1; i > 0; i--)
+                {                   
+                    solutionForDFS[i].PrintPuzzle();
+                    
+                }
+            }
+            else
+            {
+                Console.WriteLine("No path solution is found");
+            }
+            
+        }
+    }
+}
---
+++ UniformedSearch.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Puzzle8
+{
+    class UniformedSearch
+    {
+        public UniformedSearch()
+        {
+
+        }
+
+        public List<Node> BreathFirstSearch(Node root)
+        {
+            List<Node> PathToSolution = new List<Node>();
+            List<Node> OpenList = new List<Node>();
+            List<Node> CloseList = new List<Node>();
+            int dequeue = 0;
+            int enqueue = 0;
+
+            OpenList.Add(root);
+            bool goalFound = false;
+
+            while(OpenList.Count > 0 && !goalFound)
+            {
+                Node currentNode = OpenList[0];
+                CloseList.Add(currentNode);
+                OpenList.RemoveAt(0);
+
+                currentNode.ExpandNode();
+               // currentNode.PrintPuzzle();
+
+                for(int i = 0; i<currentNode.children.Count; i++)
+                {
+                    Node currentChild = currentNode.children[i];
+                    if (currentChild.GoalTest())
+                    {
+                        Console.WriteLine("Goal Found.");
+                        goalFound = true;
+                        // trace path to root node
+                        PathTrace(PathToSolution, currentChild);
+                    }
+
+                    if (!Contains(OpenList, currentChild) && !Contains(CloseList, currentChild))/* OpenList contains currentChild ? && ClosedList contain current child*/
+                    {
+                        OpenList.Add(currentChild);
+                        enqueue++;
+                    }
+                }
+
+                dequeue++;
+            }
+            Console.WriteLine("\t ***** BFS Report ****");
+            Console.WriteLine("\tDEQUEUE: {0}", dequeue);
+            Console.WriteLine("\tENQUEUE: {0}", enqueue);
+            Console.WriteLine();
+            return PathToSolution;
+        }
+
+        public List<Node> DepthFirstSearch(Node root)
+        {
+            List<Node> PathToSolution = new List<Node>();
+            List<Node> OpenList = new List<Node>();
+            List<Node> CloseList = new List<Node>();
+            int dequeue = 0;
+            int enqueue = 0;
+
+            OpenList.Add(root);
+            bool goalFound = false;
+
+            while (OpenList.Count > 0 && !goalFound)
+            {
+                
+                Node currentNode = OpenList[0];
+                CloseList.Add(currentNode);
+                OpenList.RemoveAt(0);
+
+                currentNode.ExpandNode();
+                // currentNode.PrintPuzzle();
+
+                for (int i = 0; i < currentNode.children.Count; i++)
+                {
+                    Node currentChild = currentNode.children[i];
+                    if (currentChild.GoalTest())
+                    {
+                        Console.WriteLine("Goal Found.");
+                        goalFound = true;
+                        // trace path to root node
+                        PathTrace(PathToSolution, currentChild);
+                    }
+
+                    if (!Contains(OpenList, currentChild) && !Contains(CloseList, currentChild))/* OpenList contains currentChild ? && ClosedList contain current child*/
+                    {
+                        OpenList.Insert(0, currentChild);
+                        enqueue++;
+                    }
+                }
+                dequeue++;
+            }
+            Console.WriteLine("\t **** DFS Report ******");
+            Console.WriteLine("\tDEQUEUE: {0}" , dequeue);
+            Console.WriteLine("\tENQUEUE: {0}", enqueue);
+            Console.WriteLine();
+            return PathToSolution;
+        }
+        public void PathTrace(List<Node> path, Node n)
+        {
+            //Console.WriteLine("Tracing Path ...");
+            Node current = n;
+            path.Add(current);
+
+            while(current.parent != null)
+            {
+                current = current.parent;
+                path.Add(current);
+            }
+
+        }
+
+        public static bool Contains(List<Node> list, Node c)
+        {
+
+            bool contains = false;
+
+            for(int i = 0; i < list.Count; i++)
+            {
+                if (list[i].IsSamePuzzle(c.puzzle))
+                    contains = true;
+            }
+            return contains;
+        }
+    }
+}
